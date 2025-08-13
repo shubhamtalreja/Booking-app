86839e3dbb622b4348b958dfece666d85a6bf8f0
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-08-13 23:41:38 +0530

    Created auth controllers for register and login user
    - Created auth controllers for register and login user.
    - Added pre method in user model for
    password hashing.
    - Created matchPassword function in user model for comparing the enteredPassword hash with saved hash password.
    - Added new key JWT_SECRET in env.

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,76 @@
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user.model');
+
+
+const generateJwtToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        exp: '10d'
+    })
+}
+
+//@desc Register a new user
+//@route POST /api/auth/register
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+        res.status(400);
+        throw new Error('User with email already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateJwtToken(user._id),
+
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+
+})
+
+//@desc Register a new user
+//@route POST /api/auth/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateJwtToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+})
+
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -33,8 +33,25 @@ const userSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true
-}
-)
+});
+
+userSchema.pre('save', async function(next){
+
+    if(!this.isModified(this.password) ){
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt(this.password,salt);
+
+    next();
+ });
+
+ userSchema.methods.matchPassword = async function(enteredPassword){
+
+    return await bcrypt.compare(enteredPassword, this.password);
+ }
 
 const User = mongoose.model('User', userSchema);
 
