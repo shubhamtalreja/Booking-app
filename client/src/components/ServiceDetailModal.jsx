1ee141d8938a8cd4c0600d6639116465b818d0e1
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-09-30 23:47:05 +0530

    AB-19 Added view service details section .

--- client/src/components/ServiceDetailModal.jsx
+++ client/src/components/ServiceDetailModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const ServiceDetailModal = ({isOpen, onClose, selection}) => {
-if (!isOpen || !selection) {
+const ServiceDetailModal = ({ isOpen, onClose, selection }) => {
+  if (!isOpen || !selection) {
     return null;
   }
   return (
@@ -13,17 +13,29 @@ if (!isOpen || !selection) {
       */}
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
-          <h3>Serive Details</h3>
+          <h3>Service Details</h3>
         </div>
         <div className="modal-body">
-          <p>
-            <strong>Description:</strong> {selection.description}
-          </p>
+         
+          <div>
+            {selection.imageUrls.map((image, index) => {
+              console.log(image);
+              return (
+                <img key={index} src={image} alt={`service-${index}`} className="object-cover w-full h-100 rounded" />
+              );
+            })}
+            <p>
+              <strong>Description:</strong> {selection.description}
+            </p>
+          </div>
           <p>
             <strong>Service:</strong> {selection.name}
           </p>
           <p>
             <strong>Price:</strong> &#8377;{selection.price}
+          </p>
+          <p>
+            <strong>Rating:</strong> ****
           </p>
         </div>
         <div className="modal-footer">
