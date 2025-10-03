a669b7f0dd39d555075412818168600bbd0992d2
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-10-03 23:14:19 +0530

    Added code for get lat long

--- client/src/components/GetLocation.jsx
+++ client/src/components/GetLocation.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const GetLocation = () => {
+const Getlocation = ({ onLocation, onSelectLocation }) => {
+  const [location, setLocation] = useState(null);
+  const [selected, setSelected] = useState(false);
+
+  const handleGetLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          const loc = { lat: coords.latitude, lng: coords.longitude };
+          setLocation(loc);
+          setSelected(false);
+          onLocation && onLocation(loc);
+        },
+        () => alert('Location access failed.')
+      );
+    } else {
+      alert('Geolocation not supported.');
+    }
+  };
+
+  const handleSelectLocation = () => {
+    setSelected(true);
+    if (onSelectLocation && location) onSelectLocation(location);
+  };
+
   return (
     <div>
-      
+      <button onClick={handleGetLocation}>Get My Location</button>
+      {location && (
+        <div>
+          <p>Latitude: {location.lat}</p>
+          <p>Longitude: {location.lng}</p>
+          <button onClick={handleSelectLocation}>
+            {selected ? 'Location Selected' : 'Select Location'}
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GetLocation
+export default Getlocation;
