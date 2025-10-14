2b0ae78a8a162b1025800cb3520c959ac111c557
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-10-14 23:41:05 +0530

    Added bcrypt for vendor password hashing.

--- server/controllers/vendor.controller.js
+++ server/controllers/vendor.controller.js
@@ -8,20 +8,28 @@ const ErrorResponse = require('../utils/errorResponse');
 // @route   POST /api/vendors
 // @access  Private/Admin
 const createVendor = asyncHandler(async (req, res) => {
-    const { name, description, address, category, city, phone, imageUrls } = req.body;
+    const { name, description, email, password, address, category, city, phone, imageUrls } = req.body;
 
-    if (!name || !description || !address || !category || !city || !phone) {
-        return next(new ErrorResponse(`Please provide all required fields: name, description, address, category, city, and phone.`, 404))
+    if (!name || !description || !address || !category || !city || !phone || !password || !email) {
+        return next(new ErrorResponse(`Please provide all required fields.`, 404))
+    }
+
+    const vendorExist = await Vendor.findOne({ email })
+
+    if (vendorExist) {
+        return next(new ErrorResponse('Vendor with email already exist', 400));
     }
 
     const vendor = await Vendor.create({
         name,
+        email,
         description,
         address,
         category,
         city,
         phone,
-        imageUrls: imageUrls || []
+        imageUrls: imageUrls || [],
+        password
     })
 
     res.status(201).json({ vendor });
--- server/models/vendor.model.js
+++ server/models/vendor.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const vendorSchema = new mongoose.Schema({
     name: {
@@ -57,6 +58,24 @@ const vendorSchema = new mongoose.Schema({
     {
         timestamps: true
     })
+
+vendorSchema.pre('save', async function (next) {
+
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+vendorSchema.methods.matchPassword = async function (enteredPassword) {
+
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const Vendor = mongoose.model('Vendor', vendorSchema);
 
